ef126c6e07fc84aadfaadf1a4e2fd21a99d0af3c
juli-bun <43589086+juli-bun@users.noreply.github.com>
2018-09-30 19:11:18 +0000

    add file

---
+++ index.php
@@ -0,0 +1,18 @@
+<strong>[EN]</strong>
+
+<h1>Welcome to your new website!</h1>
+
+SBS "Web-Pro" is installed and ready for further setup.<p />
+This is temporary page of your website.<p />
+You can made all settings in
+<a href="sbs_wp_cp/">Control Panel</a>.
+
+<p />
+<strong>[RU]</strong>
+
+<h1>Добро пожаловать на ваш новый сайт!</h1>
+
+SBS "Web-Pro" установлена и готова к дальнейшей настройке.<p />
+Это временная страница вашего сайта.<p />
+Все настройки вы можете сделать в
+<a href="sbs_wp_cp/">Панели управления</a>.
